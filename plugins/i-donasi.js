bd6c1980a5e96f6396bd6abcdd27a58d61358afd
HexZaor <108601619+HexZaor@users.noreply.github.com>
2022-10-13 09:43:39 +0800

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 │ • Qris  [Chat OWNER]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/628534824252
+│ > Ingin donasi? Wa.me/6285348242252
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
