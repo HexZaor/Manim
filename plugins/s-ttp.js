cd18522275bd0e469aa888f582c84d680b7a0bf5
HexZaor <108601619+HexZaor@users.noreply.github.com>
2022-12-04 00:34:40 +0800

    Update s-ttp.js

--- plugins/s-ttp.js
+++ plugins/s-ttp.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : 'teksnya mana?'
 let sel = (`https://apimu.my.id/other/ttp?text=${teks}`)
- conn.sendFile(m.chat, sel, 'ttp.webp', 0,".s", m, {packname: packname, author: author, mentions: [m.semder]})
+ conn.sendFile(m.chat, sel, 'ttp.webp', 0, m, {packname: packname, author: author, mentions: [m.semder]})
 }
 handler.help = ['ttp <teks>']
 handler.tags = ['sticker']
